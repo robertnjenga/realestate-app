1baf1d50f87bdbb8a08a7a93467bafe649bb8da5
robert_codes <rnnjenga@gmail.com>
2023-04-29 10:26:16 -0700

    added .env

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,7 +5,7 @@ export const baseUrl = 'https://unofficial-redfin.p.rapidapi.com';
 export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
-      'X-RapidAPI-Key': 'fbf0a3fa32msh4b3c24e10266376p1a8810jsnb275a077bdb6',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
       'X-RapidAPI-Host': 'unofficial-redfin.p.rapidapi.com',
     }
   });
